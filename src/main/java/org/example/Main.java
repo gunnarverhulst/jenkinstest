4e3775fb07d6a10d39a03c10b347ae6740c200b8
gunnarverhulst <verhulstg@hotmail.com>
2023-12-24 22:59:26 +0100

    changes to represent a small rest api

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,20 @@
 package org.example;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
 public class Main {
+
+    @GetMapping("/api")
+    public String message(){
+        return "Hello world";
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        SpringApplication.run(Main.class, args);
     }
 }
